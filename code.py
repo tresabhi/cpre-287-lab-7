fa877abb7d89cf29737cd1b4a63307e6f44e0826
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-03-03 12:39:42 -0600

    max

--- code.py
+++ code.py
@@ -2,11 +2,15 @@ import time
 import board
 import analogio
 
+INPUT_VOLTAGE = 2.57
+PIN_MAX = 51000
+
 analog_pin = analogio.AnalogIn(board.A0)
 
 def get_voltage(pin):
-    return (pin.value * 2.57) / 51000
+    return (pin.value * INPUT_VOLTAGE) / PIN_MAX
 
 while True:
-    print(get_voltage(analog_pin))
+    v = get_voltage(analog_pin)
+    print(f"v = {v:.3g}V")
     time.sleep(0.1)
